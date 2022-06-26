2d7b04f1e53640e7c9d93851754994f1494e5435
TheSubtleARtist <thesubtleartist@yahoo.com>
2022-06-26 00:53:20 -0500

    noComment

--- module_05/mongodb_test.py
+++ module_05/mongodb_test.py
@@ -2,8 +2,8 @@ from pymongo import MongoClient
 from pprint import pprint
 mongoUname = 'ViktorS'
 mongoPword = 'D3adFr0g2'
-cluster = MongoClient(f'mongodb+srv://{mongoUname}:{mongoPword}@cluster0.qhciunv.mongodb.net/?retryWrites=true&w=majority')
-db = cluster.pytech
+client = MongoClient(f'mongodb+srv://{mongoUname}:{mongoPword}@cluster0.qhciunv.mongodb.net/?retryWrites=true&w=majority')
+db = client.pytech
 print('--Pytech Collection List--', '\n', db.list_collection_names())
 
 
