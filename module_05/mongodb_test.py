505464150305e6b12797d22c66f1bfd861be5526
TheSubtleARtist <thesubtleartist@yahoo.com>
2022-06-26 02:07:18 -0500

    none

--- module_05/mongodb_test.py
+++ module_05/mongodb_test.py
@@ -4,6 +4,8 @@ mongoUname = 'ViktorS'
 mongoPword = 'D3adFr0g2'
 client = MongoClient(f'mongodb+srv://{mongoUname}:{mongoPword}@cluster0.qhciunv.mongodb.net/?retryWrites=true&w=majority')
 db = client.pytech
+
 print('--Pytech Collection List--', '\n', db.list_collection_names())
+if input('End of program, press any key to exit...'): exit(0)
 
 
--- module_05/pytech_insert.py
+++ module_05/pytech_insert.py
@@ -1,10 +1,5 @@
-'''
 
-'''
-
-
-
-from pymongo import MongoClient
+from pymongo import InsertOne, MongoClient
 from pprint import pprint
 mongoUname = 'ViktorS'
 mongoPword = 'D3adFr0g2'
@@ -12,13 +7,14 @@ client = MongoClient(f'mongodb+srv://{mongoUname}:{mongoPword}@cluster0.qhciunv.
 #db = client.pytech
 pydb = client['pytech']
 pycol = pydb['students']
-stuList = [
-{'stu_id': 1007, 'fname':'thorin', 'lname':'oakenshield' },
-{'stu_id': 1008, 'fname':'frodo', 'lname':'baggins' },
-{'stu_id': 1009, 'fname':'bilbo', 'lname':'baggins' }
-]
+
+stu_id=[1007, 1008, 1009]
+fnames = ['thorin', 'bilbo', 'frodo']
+lnames = ['oakenshield', 'baggins', 'baggins']
 
 print('---Insert Statements---')
-print(f'Inserted student record {fname.title()} {lname.title()} into the students collection with document_id {doc_id}')
-print('End of program, press any key to exit')
+for i in range(len(stu_id)):
+    doc = pycol.insert_one({'stu_id':stu_id[i], 'stu_fname':fnames[i], 'stu_lname':lnames[i]})
+    print(f'Inserted student record {fnames[i].title()} {lnames[i].title()} into the students collection with document_id {doc.inserted_id}')
+if input('End of program, press any key to exit...'): exit(0)
 
