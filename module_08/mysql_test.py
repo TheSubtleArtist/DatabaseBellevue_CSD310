a21bf695e6ec658045a712bd905f69ae82e6259e
TheSubtleARtist <thesubtleartist@yahoo.com>
2022-07-14 19:17:52 -0500

    none

---
+++ module_08/mysql_test.py
@@ -0,0 +1,25 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+config = {
+    'user'             :'pysports_user',
+    'password'         :'MySQL8IsGreat!',
+    'host'             :'127.0.0.1',
+    'database'         :'pysports',
+    'raise_on_warnings':True
+}
+
+if __name__ == '__main__':
+    try:
+        db = mysql.connector.connect(**config)
+        print('\n Database user {} connected to MySQL on host {} wiht database {}.'.format(config['user'], config['host'], config['database']))
+        input('\n\n Press any key to continue...')
+    except mysql.connector.Error as err:
+        if err.errorno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print('The supplied username or password is invalid.')
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print('The specified database does not exist')
+        else:
+            print(err)
+    finally:
+        db.close()
