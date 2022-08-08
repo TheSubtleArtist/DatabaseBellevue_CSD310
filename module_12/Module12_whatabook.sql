5ed1e705446460c679d6e620016ad35b427ca29c
TheSubtleARtist <thesubtleartist@yahoo.com>
2022-08-08 14:12:05 -0500

    None

--- module_12/Module12_whatabook.sql
+++ module_12/Module12_whatabook.sql
@@ -1,11 +1,15 @@
+DROP DATABASE IF EXISTS whatabook;
+DROP USER IF EXISTS 'whatabook_user'@'localhost'; 
+
 -- create new database 'whatabook'
 CREATE DATABASE whatabook;
+USE whatabook;
 
 -- create whatabook_user and grant them all privileges to the pysports database 
 CREATE USER 'whatabook_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'MySQL8IsGreat!';
 
 -- grant all privileges to the pysports database to user pysports_user on localhost 
-GRANT ALL PRIVILEGES ON whatabook.* TO'pysports_user'@'localhost';
+GRANT ALL PRIVILEGES ON whatabook.* TO'whatabook_user'@'localhost';
 
 -- create the store table 
 CREATE TABLE store (
@@ -27,7 +31,10 @@ CREATE TABLE book (
     book_name   VARCHAR(200)    NOT NULL,
     book_author VARCHAR(200)    NOT NULL,
     book_detail VARCHAR(500),
-    PRIMARY KEY(book_id)
+    store_id	INT DEFAULT 1				NOT NULL,
+    PRIMARY KEY(book_id),
+	CONSTRAINT store_id FOREIGN KEY(store_id) REFERENCES store(store_id)
+    
 ); 
 
 -- create the wishlist table and set the foreign keys
@@ -54,19 +61,21 @@ INSERT INTO user_table (first_name, last_name) VALUES
     ('dawn', 'ardent');
     
 INSERT INTO book (book_author, book_name, book_detail) VALUES
-	('robert a. heinlein', 'stranger in a string land', 'if you grok it, they will come'),
+	('robert heinlein', 'stranger in a string land', 'if you grok it, they will come'),
     ('robert a. heinlein', 'starship troopers', 'service guarantees citizenship'),
-    ('robert a. heinlein', 'the moon is a harsh mistress', 'insurrection a-z'),
-    ('robert a. heinlein', 'time enough for love', 'earn it to earn it'),
+    ('robert anson heinlein', 'the moon is a harsh mistress', 'insurrection a-z'),
+    ('robert heinlein', 'time enough for love', 'earn it to earn it'),
     ('robert a. heinlein', 'job: a comedy of justice', 'never meet your heros or your enemies'),
-    ('robert a. heinlein', 'to sail beyond the sunset', 'let me try this again'),
-    ('robert a. heinlein', 'i will fear no evil', 'thank you, eunice'),
+    ('robert anson heinlein', 'to sail beyond the sunset', 'let me try this again'),
+    ('robert heinlein', 'i will fear no evil', 'thank you, eunice'),
     ('robert a. heinlein', 'the man who sold the moon ', 'there really is a sucker born every minute'),
-    ('robert a. heinlein', 'methusala\'s children', 'it already happened tomorrow');
+    ('robert anson heinlein', 'methusala\'s children', 'it already happened tomorrow');
     
     -- determine primary key values assigned to users and books
     select * from user_table;
-    select* from book;
+    select * from book;
+    select * from store;
+    
     
 -- create wishlist
 INSERT INTO wishlist (user_id, book_id) VALUES
@@ -78,7 +87,19 @@ INSERT INTO wishlist (user_id, book_id) VALUES
     (3, 6);
 
 
-    
+ALTER TABLE book ADD COLUMN store INT NOT NULL AFTER book_detail;
+ALTER TABLE book
+ADD CONSTRAINT fk_store
+FOREIGN KEY (store) REFERENCES store(store_id);
+
+ALTER TABLE store ADD COLUMN open_time VARCHAR(12) NOT NULL AFTER locale;
+ALTER TABLE store ADD COLUMN close_time VARCHAR(12) NOT NULL AFTER open_time;
+
+UPDATE store SET open_time = '24 Hrs', close_time = '24 Hrs' where store_id = 1;
+
+INSERT INTO store (locale, open_time, close_time) VALUES ('ogalalla', '9 AM', '9 PM');
+
+
     
 
 
--- module_12/whatabook.py
+++ module_12/whatabook.py
@@ -30,18 +30,133 @@ def dbConnect(connect):
             print(err)
 
 # print all books
-
-def testFunctions(cursor):
-    printBooks(cursor)
-
-def printBooks(cursor):
-    cursor.execute('show books')
+def show_books(cursor):
+    print('--DISPLAYING ALL BOOKS--')
+    cursor.execute('SELECT * FROM book INNER JOIN store ON book.store_id')
     for record in cursor:
-        print(record)
+        #print(record)
+        print(f"Book ID: {record[0]}", f"Title: {record[1].title()}", f"Author: {record[2].title()}", f"Book Detail: {record[3].title()}", f"Location: {record[6]}", sep='\n' )
+        print('\n')
+    print('\n')
 
+# Requested user_id, validate the user_id
+def get_user(cursor):
+    # Set a flag to create a loop to go through User ID until a valid User ID is input
+    flag = False
+    # Enter the while loop
+    while flag == False:
+        print('\n','--DISPLAYING ALL USERS-- ')
+        cursor.execute('SELECT * FROM user_table')
+        for u in cursor:
+            #print(record)
+            print(f"User ID: {u[0]}", f"Name: {u[1].title()} {u[2].title()}", sep="  ")
+            #print('\n')
+        # request user id input    
+        requested_user = input('Enter a valid user ID: ')
+        if int(requested_user) < 0 or int(requested_user) >3:
+            print('Invalid User ID. Please try again', '\n')
+        else:
+            flag = True
+        return requested_user
+
+# Show specific user information
+def display_user(cursor):
+    requested_user = get_user(cursor)
+    user_request = 'SELECT * FROM user_table WHERE user_id = %s'
+    cursor.execute(user_request, (requested_user,))
+    u = cursor.fetchone()
+    print('---User Details---')
+    print(f"User ID: {u[0]}", f"Name: {u[1].title()} {u[2].title()}", sep="  ")
+    return requested_user
+    
+def display_wishlist(cursor):
+    requested_user = display_user(cursor)
+    #print(requested_user)
+    wishlist_request = ("SELECT * FROM user_table INNER JOIN wishlist ON user_table.user_id = wishlist.user_id INNER JOIN book ON wishlist.book_id = book.book_id WHERE user_table.user_id = %s")
+    cursor.execute(wishlist_request, (requested_user,))
+    wishlist = cursor.fetchall()
+    print('\n', '--- User Wishlist---')
+    for r in wishlist:
+        print(f"Title: {r[7].title()}",f"Author: {r[8].title()}", f"Book Detail: {r[9].title()}", sep='\n')
+        print('\n')
+
+def books_to_add(cursor):
+    requested_user = get_user(cursor)
+    available_books_request = ("SELECT book_id, book_name, book_author FROM book WHERE book_id NOT IN (SELECT book_id FROM wishlist WHERE user_id = %s)")
+    cursor.execute(available_books_request, (requested_user,))
+    available_books = cursor.fetchall()
+    print('\n', '--Books available for addition to the user wishlist--')
+    #print(available_books)
+    for r in available_books:
+        print(f"Book ID: {r[0]}", f"Book Title: {r[1].title()}", f"Author: {r[2].title()}", sep="\n")
+        print('\n')
+
+def add_to_wishlist(cursor, user, book):
+    add_statement = ("INSERT INTO wishlist(user_id, book_id) VALUES(%s, %s )")
+    cursor.execute(add_statement, user, book)
+    
+
+# show all stores
+def show_stores(cursor):
+    print('\n', '--DISPLAYING STORE LOCATIONS--')
+    cursor.execute('SELECT * FROM store')
+    for record in cursor:
+        print(f"Store {record[0]}: {record[1].title()}")
+    print('\n')
+
+# Exit the program
+def exit_program(cursor):
+    print('All Hail The Google!')
+    exit(0)
+
+# Show a menu
+def show_menu(menu):
+    print('\n', 'Action Menu')
+    for key, value in menu.items():
+        print(f'{key}: {value.title()}')
+
+def main(cursor):
+    show_menu(main_menu)
+    main_option = input("Please select an option: ")
+    function_list[main_option](whatabookcursor)
+
+def user(cursor):
+    # display the user account options menu
+    show_menu(user_menu)
+    user_option = input('Enter an option: ')
+    function_list[user_option](cursor)
+
+main_menu = {
+    "1": "show books",
+    "2": "show store locations",
+    "3": "User Account",
+    "0": "exit"
+}
+
+function_list = {
+    '1': show_books,
+    '2': show_stores,
+    '3': user,
+    '4': display_user,
+    '5': display_wishlist,
+    '6': books_to_add,
+    '9': main,
+    '0': exit_program
+}
+
+user_menu = {
+    '4': 'show user data',
+    '5': "show user wishlist",
+    '6': "add book to wishlist",
+    '9': "return to main menu"
+}
 
 if __name__ == '__main__':
-    testFunctions(whatabookcursor)
+    print('Welcome to What a Book', '\n')
+    while True:
+        main(whatabookcursor)
 
-    #input('Press any key to continue...')
-    #exit(0)
+
+
+
+
